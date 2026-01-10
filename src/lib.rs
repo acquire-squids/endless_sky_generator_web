3c5cd67a64675b5647ad9e888c98b94dbad757e3
acquire-squids <62000632+acquire-squids@users.noreply.github.com>
2026-01-09 20:07:21 -0500

    Move generators to their own module for organization

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
-mod full_map;
+mod generators;
 mod import_from_javascript;
-mod system_shuffler;
 mod wandom;
 mod zippy;
 
@@ -8,14 +7,14 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn generate_full_map(paths: Vec<String>, sources: Vec<String>) -> Result<Vec<u8>, String> {
-    self::full_map::process(paths, sources).map_err(|e| e.to_string())
+    self::generators::full_map::process(paths, sources).map_err(|e| e.to_string())
 }
 
 #[wasm_bindgen]
 pub fn generate_system_shuffler(
     paths: Vec<String>,
     sources: Vec<String>,
-    settings: self::system_shuffler::SystemShufflerConfig,
+    settings: self::generators::system_shuffler::SystemShufflerConfig,
 ) -> Result<Vec<u8>, String> {
-    self::system_shuffler::process(paths, sources, settings).map_err(|e| e.to_string())
+    self::generators::system_shuffler::process(paths, sources, settings).map_err(|e| e.to_string())
 }
